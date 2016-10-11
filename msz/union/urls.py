27ef62eeeba8c74ef30551389ed7b11a831592b7
xuchao <xuchao_666@163.com>
2016-10-11 18:19:08 +0800

    login

--- msz/union/urls.py
+++ msz/union/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'msz.union.views',
-    url(r'^/$', 'login', name='login'),
+    url(r'^login/$', 'login', name='login'),
     url(r'^logout/$', 'logout', name='logout'),
 )
